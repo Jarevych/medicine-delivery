4122c3bbdc9460fd689b53ba58f10ce6f4b3dcaa
Yaroslav Grabovych <ygrabovych@gmail.com>
2024-03-03 00:54:42 +0200

    add remove button

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 
  const ShoppingCart = ({ added }) => {
    
@@ -8,13 +8,18 @@ import { useLocation } from 'react-router-dom';
         // console.log(products)
         const [cart, setCart] = useState([])
     const defaultImg = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Medicine_Drugs.svg/2560px-Medicine_Drugs.svg.png'
-
+    console.log(cart)
     useEffect(() => {
         const savedItem = localStorage.getItem('cart');
         if(savedItem) {
             setCart(JSON.parse(savedItem))
         }
     },[])
+    const deleteItem = (productId) => {
+       const updateCart = cart.filter(item => item.id !== productId)
+       setCart(updateCart)
+       localStorage.setItem('cart', JSON.stringify(updateCart))
+    }
 
 return (
     <div>
@@ -23,6 +28,7 @@ return (
                 <li key={product.id}>
                     <p>{product.name}</p>
                     <img src={defaultImg} alt="defImg" style={{ width: "140px" }}/>
+                    <button onClick={() => deleteItem(product.id)}>delete</button>
                 </li>
             ))}
         </ul>
