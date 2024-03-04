e667ff5eef18c60fa549fc76ac185d63bb281b58
Yaroslav Grabovych <ygrabovych@gmail.com>
2024-03-04 20:37:36 +0200

    add order handler

--- src/api.js
+++ src/api.js
@@ -1,12 +1,31 @@
 import axios from 'axios'
 
-
+const BASE_URL = 'http://localhost:3001/api'
 
 export async function fetchStores() {
 
+    try {
+        const response = await axios.get(`${BASE_URL}/pharmacies`)
+
+        if(response.status !== 200) {
+            throw new Error('THere is no result')
+        }
+        console.log(response.data)
+        return response.data
+    } catch (error) {
+        throw new Error('There is no result for your query');
+
+    }
+}
+
+export async function addOrderHandler(formData) {
 
     try {
-        const response = await axios.get('https://65e3847288c4088649f5e0a4.mockapi.io/api/Pharm')
+        const response = await axios.post(`${BASE_URL}/orders`, formData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
 
         if(response.status !== 200) {
             throw new Error('THere is no result')
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -1,97 +1,96 @@
+import { addOrderHandler } from 'api';
 import { OrderStyled } from './OrderStyled';
 import React, { useState } from 'react';
 
+const Order = ({ cart }) => {
+  // const [checkedItems, setCheckedItems] = useState([cart])
+  const [formData, setFormData] = useState({
+    name: null,
+    phone: null,
+    email: null,
+    address: null,
+    drags: null,
+  });
 
-const Order = ({cart}) => {
-    // const [checkedItems, setCheckedItems] = useState([cart])
-    const [formData, setFormData] = useState({
-        name: null,
-        phone: null,
-        email: null,
-        address: null,
-        drags: null
-    })
+  console.log(cart);
+  // const orederedItems = localStorage.getItem(JSON.parse 'cart');
 
-    console.log(cart)
-    // const orederedItems = localStorage.getItem(JSON.parse 'cart');
+  const handleChange = e => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormData(prevData => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData({
+      name: '',
+      phone: '',
+      email: '',
+      address: '',
+      drugs: cart,
+    });
+    addOrderHandler(formData);
+  };
 
-
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        console.log(name, value)
-        setFormData(prevData => ({
-            ...prevData, 
-            [name]: value
-        })); 
-    }
-
-    const handleSubmit =(e) => {
-        e.preventDefault()
-        console.log(formData);
-        setFormData({
-            name: '',
-        phone: '',
-        email: '',
-        address: '',
-        drugs: cart
-        })
-    }
-
-    return (
-        <OrderStyled>
-        <form className='form' onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          className='input'
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="phone">Phone:</label>
-        <input
-          className='input'
-          type="tel"
-          id="phone"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">E-mail:</label>
-        <input
-          className='input'
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="address">Address:</label>
-        <textarea
-          className='input'
-          id="address"
-          name="address"
-          value={formData.address}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <button type="submit">Buy</button>
-    </form>
+  return (
+    <OrderStyled>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            className="input"
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input
+            className="input"
+            type="tel"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="email">E-mail:</label>
+          <input
+            className="input"
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="address">Address:</label>
+          <textarea
+            className="input"
+            id="address"
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button type="submit">Buy</button>
+      </form>
     </OrderStyled>
-    )
-}
+  );
+};
 
 export default Order;
